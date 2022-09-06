648fba3f4a8e297d59278834a4d071020c407445
Elijah-Harvey <theeelijahharvey@gmail.com>
2022-09-05 20:38:21 -0400

    Finished Uber2.0

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import 'react-native-gesture-handler';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import MapScreen from './src/screens/MapScreen';
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <TailwindProvider>
+            <StatusBar style="auto" />
+            <KeyboardAvoidingView
+              className="flex-1"
+              behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+              keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+            >
+              <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="HomeScreen" component={HomeScreen} />
+                <Stack.Screen name="MapScreen" component={MapScreen} />
+              </Stack.Navigator>
+            </KeyboardAvoidingView>
+          </TailwindProvider>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
